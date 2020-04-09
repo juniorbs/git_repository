3c881f6e50ec8e30393aa50e201ea66194d69ed7
Gilman Junior <gilman.batista@ibm.com>
2020-04-09 15:18:37 -0300

    Primeiro commit - CURSOJS

---
+++ CURSOJS/AULA11/ex007.js
@@ -0,0 +1,10 @@
+var vel = 60.5
+console.log (`A velocidade do seu carro é ${vel}KM/H`) 
+if (vel > 70){
+    console.log(`Vc ultrapassou a velocidade permitida`)
+}
+else{
+    console.log(`Vc esta dirigindo dentro da velocidade permitida`)
+}
+console.log(`Dirija sempre usando o cinto de segurança`)
+
